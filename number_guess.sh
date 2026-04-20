36e1ebf27fae8d3d998a351feb4ebd5f20d0e38d
Nena Kozić <148547388+NenaK3@users.noreply.github.com>
2026-04-20 15:31:41 +0200

    Add files via upload

---
+++ number_guess.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "Enter your username:"
+read USERNAME
+
+USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+else
+  echo "$USER_INFO" | while IFS="|" read GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+echo "Guess the secret number between 1 and 1000:"
+
+GUESS_COUNT=0
+while true
+do
+  read GUESS
+  (( GUESS_COUNT++ ))
+
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  if [[ $GUESS -eq $SECRET_NUMBER ]]
+  then
+   
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+    
+    USER_STATS=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+    echo "$USER_STATS" | while IFS="|" read G_PLAYED B_GAME
+    do
+      NEW_GAMES_PLAYED=$(( G_PLAYED + 1 ))
+      
+      if [[ -z $B_GAME || $GUESS_COUNT -lt $B_GAME ]]
+      then
+        UPDATE_RES=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED, best_game=$GUESS_COUNT WHERE username='$USERNAME'")
+      else
+        UPDATE_RES=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED WHERE username='$USERNAME'")
+      fi
+    done
+    
+    break
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
